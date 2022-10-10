6a38c4c6193b9ddcb9a8157aa7cc934682d2c2bd
dgouron <damien.gouron@gmail.com>
2022-10-10 18:04:51 +0200

    Add projects

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -12,7 +12,7 @@ export const ProjectsData = [
     projectLink: 'https://more-rhum.netlify.app/',
     projectDescription: 'Jeu par navigateur de type cookie clicker.',
     projectLargeImage: '/assets/thumbnails/Large_morerhum.png',
-    projectDetails: ['React', 'Redux-toolkit'],
+    projectDetails: ['- Front avec React', '- Utilisation de Redux-toolkit'],
   },
   {
     projectName: 'Learn Suap',
@@ -20,30 +20,11 @@ export const ProjectsData = [
     projectDescription:
       "Création d'un quizz interactif sur le thème du secours d'urgence aux personnes.",
     projectLargeImage: '/assets/thumbnails/Large_LearnSuap.png',
-    projectDetails: ['React', 'Redux', 'Router'],
-  },
-  {
-    projectName: 'Damien Gouron - Portfolio',
-    projectLink: 'https://dgouron.github.io/',
-    projectDescription: 'Ancien portfolio. Fait entièrement en HTML/CSS/JS Vanilla.',
-    projectLargeImage: '/assets/thumbnails/Large_OldPortfolio.png',
-    projectDetails: ['Github', 'Javascript'],
-  },
-  {
-    projectName: 'Arkas Dev - Portfolio',
-    projectLink: 'https://arkasdev.wixsite.com/home',
-    projectDescription:
-      'Site de présentation de mes travaux dans le jeux vidéo: création de menu/UI, gameplay multijoueurs, heatmap en temps réel, API REST, etc.',
-    projectLargeImage: '/assets/thumbnails/Large_ArkasDev.jpg',
-    projectDetails: ['Wix'],
-  },
-  {
-    projectName: 'Print Message Visualizer',
-    projectLink: 'https://www.unrealengine.com/marketplace/en-US/product/print-message-visualizer',
-    projectDescription:
-      "Plugin Unreal Engine 4 fait en C++. PMV est un outil qui permet de traquer en temps réel, de sauvegarder et de comparer des données de jeu. Par exemple les points de passage des joueurs, les morts, etc. Parfais pour le debug d'un jeu multijoueur.",
-    projectLargeImage: '/assets/thumbnails/Large_PMV.jpg',
-    projectDetails: ['C++', 'Plugin UE4'],
+    projectDetails: [
+      '- Front avec React',
+      '- Utilisation de Redux',
+      '- Utilisation de Router pour le Client Side Routing',
+    ],
   },
   {
     projectName: 'Linda Keisea',
@@ -51,7 +32,23 @@ export const ProjectsData = [
     projectDescription:
       "Création d'un blog de lecture et d'écriture. Ajout des statistiques Google Site Kit. Paramétrages de l'affichage, gestion des utilisateurs avec formulaire d'adhésion au site. Prise en compte du certificat SSL pour accroître la sécurité du site.",
     projectLargeImage: '/assets/thumbnails/Large_lindakeisea.png',
-    projectDetails: ['Wordpress'],
+    projectDetails: [
+      "- Création et customation d'un site Wordpress",
+      '- Utilisation de plugins',
+      '- Modification de thème',
+    ],
+  },
+  {
+    projectName: 'Print Message Visualizer',
+    projectLink: 'https://www.unrealengine.com/marketplace/en-US/product/print-message-visualizer',
+    projectDescription:
+      "Plugin Unreal Engine 4 fait en C++. PMV est un outil qui permet de traquer en temps réel, de sauvegarder et de comparer des données de jeu. Par exemple les points de passage des joueurs, les morts, etc. Parfais pour le debug d'un jeu multijoueur.",
+    projectLargeImage: '/assets/thumbnails/Large_PMV.jpg',
+    projectDetails: [
+      '- Algorithmes C++',
+      '- Tracking des données joueurs',
+      '- Sauvegarde des données',
+    ],
   },
   {
     projectName: 'To-do list',
@@ -59,6 +56,6 @@ export const ProjectsData = [
     projectDescription:
       "Création d'une To-do list et clonage de la page principale du site Todoist.",
     projectLargeImage: '/assets/thumbnails/Large_ToDoList.png',
-    projectDetails: ['React'],
+    projectDetails: ['- Front avec React'],
   },
 ];
