82d1c9db60984482a2560005a39a06ac8cf906ec
dgouron <damien.gouron@gmail.com>
2022-10-10 15:08:36 +0200

    add cta button in the header for desktop only

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from 'react';
+import CallToAction from '../buttons/CallToAction';
 
 function Header() {
   return (
     <header>
-        <p>Damien Gouron</p>
-        <h1>Développeur fullstack</h1>
-        <h2>« Après plusieurs années dans le développement de jeux vidéos, spécialisé dans les jeux de
-          survie et de tir multijoueurs, je me suis tourné vers le développement web. Trentenaire
-          autodidacte, j'ai pour passion de faire d’une vision projet une véritable application avec
-          comme valeur ajoutée l’interaction avec les utilisateurs.»</h2>
+      <p>Damien Gouron</p>
+      <h1>Développeur fullstack</h1>
+      <h2>
+        « Après plusieurs années dans le développement de jeux vidéos, spécialisé dans les jeux de
+        survie et de tir multijoueurs, je me suis tourné vers le développement web. Trentenaire
+        autodidacte, j'ai pour passion de faire d’une vision projet une véritable application avec
+        comme valeur ajoutée l’interaction avec les utilisateurs.»
+      </h2>
+      {window.screen && window.screen.width > 768 ? (
+        <CallToAction text={'Me contacter'} action={'mail'} />
+      ) : null}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
