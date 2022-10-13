5044ddbb5f0e8181df1f7c5e3ddaebcaad82d79a
dgouron <damien.gouron@gmail.com>
2022-10-13 14:41:49 +0200

    add some skills

--- src/components/skills/HardSkillItem.jsx
+++ src/components/skills/HardSkillItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function hardDSkillItem({ skillName, skillIcon, skillIconAlt }) {
   return (
-    <article>
+    <article className='hardskill__item--container'>
       <h2 className='hardskill__item--title'>
         <img src={skillIcon} alt={skillIconAlt} className='hardskill__item--icon' /> {skillName}
       </h2>
